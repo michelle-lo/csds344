fa31fbcbf676d28d72543646b9cbc40d22e2e505
Laura Z <122897510+lwz9LauraZ@users.noreply.github.com>
2024-02-23 19:56:13 -0500

    Finished left turn

--- intersection.py
+++ intersection.py
@@ -29,6 +29,7 @@ road = (80,80,80)
 go_time = 6000 #3000
 yellow_time = 2000 #1000
 pedestrian_time = 2000 #1000
+turn_time = 3000
 
 # Create the window
 window = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -47,6 +48,7 @@ class Light:
         self.draw()
     def draw(self):
         pygame.draw.circle(window, self.color, (self.x, self.y), self.radius)
+        
 
 def draw_dotted_line(start, end, width = 2, dash_length = 10):
     x1, y1 = start
@@ -84,12 +86,12 @@ def draw_intersection():
     draw_dotted_line((WIDTH, HEIGHT/2 - 30), (WIDTH/2 + 100, HEIGHT/2 - 30)) # Right
 
     x_shift =  WIDTH/2 - 250 
-    y_shift = HEIGHT/2 - 10
-    xs2 =  WIDTH/2 + 10
+    y_shift = HEIGHT/2 - 15
+    xs2 =  WIDTH/2 + 15
     ys2 = HEIGHT/2 - 250
     xs3 =  WIDTH/2 + 250
-    ys3 = HEIGHT/2 +10
-    xs4 =  WIDTH/2 - 10
+    ys3 = HEIGHT/2 +15
+    xs4 =  WIDTH/2 - 15
     ys4 = HEIGHT/2 + 250
 
     #Arrows
@@ -113,20 +115,27 @@ def draw_intersection():
 def draw_traffic_lights():
     # North
     pygame.draw.rect(window, black, (WIDTH/2 - 25, HEIGHT/2 - 160, 50, 120))
-    pygame.draw.rect(window, black, (WIDTH/2 + 200, HEIGHT/2 + 200, 50, 120))
+    pygame.draw.rect(window, black, (WIDTH/2  - 90, HEIGHT/2 - 160, 50, 120))
+    # pygame.draw.rect(window, black, (WIDTH/2 + 200, HEIGHT/2 + 200, 50, 120))
+    xs1 = WIDTH/2 - 15
+    ys1 = HEIGHT/2 - 75
+    pygame.draw.polygon(window, gray, ((0+xs1,10+ys1),(0+xs1,20+ys1),(20+xs1,20+ys1),(20+xs1,25+ys1),(30+xs1,15+ys1),(20+xs1,5+ys1),(20+xs1,10+ys1)))
 
     # South
     pygame.draw.rect(window, black, (WIDTH/2 - 25, HEIGHT/2 + 40, 50, 120))
-    pygame.draw.rect(window, black, (WIDTH/2 - 250, HEIGHT/2 - 320, 50, 120))
+    pygame.draw.rect(window, black, (WIDTH/2 + 40, HEIGHT/2 + 40, 50, 120))
+    # pygame.draw.rect(window, black, (WIDTH/2 - 250, HEIGHT/2 - 320, 50, 120))
 
     # West
     pygame.draw.rect(window, black, (WIDTH/2 - 160, HEIGHT/2 - 25, 120, 50))
-    pygame.draw.rect(window, black, (WIDTH/2 + 200, HEIGHT/2 - 250, 120, 50))
+    pygame.draw.rect(window, black, (WIDTH/2 - 160, HEIGHT/2 + 40, 120, 50))
+    # pygame.draw.rect(window, black, (WIDTH/2 + 200, HEIGHT/2 - 250, 120, 50))
 
 
     # East
     pygame.draw.rect(window, black, (WIDTH/2 + 40, HEIGHT/2 - 25, 120, 50))
-    pygame.draw.rect(window, black, (WIDTH/2 - 320, HEIGHT/2 + 200, 120, 50))
+    pygame.draw.rect(window, black, (WIDTH/2 + 40, HEIGHT/2 - 90, 120, 50))
+    # pygame.draw.rect(window, black, (WIDTH/2 - 320, HEIGHT/2 + 200, 120, 50))
     
 
 # Run the intersection simulation
@@ -139,20 +148,31 @@ def run_intersection():
     north_yellow = Light(gray, WIDTH/2, HEIGHT/2 - 100)
     north_green = Light(gray, WIDTH/2, HEIGHT/2 - 140)
 
+    #North Arrow
+    north_back_red = Light(gray, WIDTH/2  - 65, HEIGHT/2 - 60)
+    north_back_yellow = Light(gray, WIDTH/2  - 65, HEIGHT/2 - 100)
+    north_back_green = Light(gray, WIDTH/2  - 65, HEIGHT/2 - 140)
+
     #North Backup
-    north_back_red = Light(gray, WIDTH/2 + 225, HEIGHT/2 + 220)
-    north_back_yellow = Light(gray, WIDTH/2 + 225, HEIGHT/2 + 260)
-    north_back_green = Light(gray, WIDTH/2 + 225, HEIGHT/2 + 300)
+    # north_back_red = Light(gray, WIDTH/2 + 225, HEIGHT/2 + 220)
+    # north_back_yellow = Light(gray, WIDTH/2 + 225, HEIGHT/2 + 260)
+    # north_back_green = Light(gray, WIDTH/2 + 225, HEIGHT/2 + 300)
+
 
     # South
     south_red = Light(gray, WIDTH/2, HEIGHT/2 + 60)
     south_yellow = Light(gray, WIDTH/2, HEIGHT/2 + 100)
     south_green = Light(gray, WIDTH/2, HEIGHT/2 + 140)
 
+    # South Arrow
+    south_back_red = Light(gray, WIDTH/2 + 65, HEIGHT/2 + 60)
+    south_back_yellow = Light(gray, WIDTH/2 + 65, HEIGHT/2 + 100)
+    south_back_green = Light(gray, WIDTH/2 + 65, HEIGHT/2 + 140)
+
     # South Backup
-    south_back_red = Light(gray, WIDTH/2 - 225, HEIGHT/2 - 220)
-    south_back_yellow = Light(gray, WIDTH/2 - 225, HEIGHT/2 - 260)
-    south_back_green = Light(gray, WIDTH/2 - 225, HEIGHT/2 - 300)
+    # south_back_red = Light(gray, WIDTH/2 - 225, HEIGHT/2 - 220)
+    # south_back_yellow = Light(gray, WIDTH/2 - 225, HEIGHT/2 - 260)
+    # south_back_green = Light(gray, WIDTH/2 - 225, HEIGHT/2 - 300)
 
 
     # West
@@ -160,10 +180,15 @@ def run_intersection():
     west_yellow = Light(gray, WIDTH/2 - 100, HEIGHT/2)
     west_green = Light(gray, WIDTH/2 - 140, HEIGHT/2)
 
-    # West Backup
-    west_back_red = Light(gray, WIDTH/2 + 220, HEIGHT/2 - 225)
-    west_back_yellow = Light(gray, WIDTH/2 + 260, HEIGHT/2 - 225)
-    west_back_green = Light(gray, WIDTH/2 + 300, HEIGHT/2 - 225)
+    # West Arrow
+    west_back_red = Light(gray, WIDTH/2 - 60, HEIGHT/2 + 65)
+    west_back_yellow = Light(gray, WIDTH/2 - 100, HEIGHT/2 + 65)
+    west_back_green = Light(gray, WIDTH/2 - 140, HEIGHT/2 + 65)
+
+    # # West Backup
+    # west_back_red = Light(gray, WIDTH/2 + 220, HEIGHT/2 - 225)
+    # west_back_yellow = Light(gray, WIDTH/2 + 260, HEIGHT/2 - 225)
+    # west_back_green = Light(gray, WIDTH/2 + 300, HEIGHT/2 - 225)
 
 
     # East
@@ -171,10 +196,15 @@ def run_intersection():
     east_yellow = Light(gray, WIDTH/2 + 100, HEIGHT/2)
     east_green = Light(gray, WIDTH/2 + 140, HEIGHT/2)
 
+    # East Arrow
+    east_back_red = Light(gray, WIDTH/2 + 60, HEIGHT/2 - 65)
+    east_back_yellow = Light(gray, WIDTH/2 + 100, HEIGHT/2 - 65)
+    east_back_green = Light(gray, WIDTH/2 + 140, HEIGHT/2 - 65)
+
     # East Backup
-    east_back_red = Light(gray, WIDTH/2 - 220, HEIGHT/2 + 225)
-    east_back_yellow = Light(gray, WIDTH/2 - 260, HEIGHT/2 + 225)
-    east_back_green = Light(gray, WIDTH/2 - 300, HEIGHT/2 + 225)
+    # east_back_red = Light(gray, WIDTH/2 - 220, HEIGHT/2 + 225)
+    # east_back_yellow = Light(gray, WIDTH/2 - 260, HEIGHT/2 + 225)
+    # east_back_green = Light(gray, WIDTH/2 - 300, HEIGHT/2 + 225)
 
     # Pedestrian Lights
     southwest_vertical = pygame.Rect(WIDTH/2 - 225, HEIGHT/2 + 100, 80, 20)
@@ -199,12 +229,24 @@ def run_intersection():
         if (current == 0): # All Red
             north_red.set_color(red_on)
             south_red.set_color(red_on)
+            xs1 = WIDTH/2 - 15
+            ys1 = HEIGHT/2 - 75
+            pygame.draw.polygon(window, black, ((0+xs1,10+ys1),(0+xs1,20+ys1),(20+xs1,20+ys1),(20+xs1,25+ys1),(30+xs1,15+ys1),(20+xs1,5+ys1),(20+xs1,10+ys1)))
+            xs1 = WIDTH/2 + 14
+            ys1 = HEIGHT/2 + 45
+            pygame.draw.polygon(window, black, ((0+xs1,10+ys1),(0+xs1,20+ys1),(-20+xs1,20+ys1),(-20+xs1,25+ys1),(-30+xs1,15+ys1),(-20+xs1,5+ys1),(-20+xs1,10+ys1)))
 
             north_back_red.set_color(red_on)
             south_back_red.set_color(red_on)
 
             west_red.set_color(red_on)
             east_red.set_color(red_on)
+            xs1 = WIDTH/2 - 75
+            ys1 = HEIGHT/2 - 15
+            pygame.draw.polygon(window, black, ((10+xs1,0+ys1),(20+xs1,0+ys1),(20+xs1,20+ys1),(25+xs1,20+ys1),(15+xs1,30+ys1),(5+xs1,20+ys1),(10+xs1,20+ys1)))
+            xs1 = WIDTH/2 + 75
+            ys1 = HEIGHT/2 - 15
+            pygame.draw.polygon(window, black, ((-10+xs1,0+ys1),(-20+xs1,0+ys1),(-20+xs1,20+ys1),(-25+xs1,20+ys1),(-15+xs1,30+ys1),(-5+xs1,20+ys1),(-10+xs1,20+ys1)))
 
             west_back_red.set_color(red_on)
             east_back_red.set_color(red_on)
@@ -215,6 +257,7 @@ def run_intersection():
             west_back_yellow.set_color(gray)
             east_back_yellow.set_color(gray)
             
+            # Pedestrian Lights
             pygame.draw.rect(window, red_on, southwest_vertical)
             pygame.draw.rect(window, red_on, northwest_vertical)
             pygame.draw.rect(window, red_on, southeast_vertical)
@@ -227,19 +270,60 @@ def run_intersection():
             current = 1
             delay = pedestrian_time
 
-        elif (current == 1): #North and South Green
-            north_green.set_color(green_on)
-            south_green.set_color(green_on)
-
-            north_back_green.set_color(green_on)
-            south_back_green.set_color(green_on)
-
+        elif (current == 1): # vertical LT green
             north_red.set_color(gray)
             south_red.set_color(gray)
+            north_green.set_color(green_on)
+            south_green.set_color(green_on)
+            xs3 = WIDTH/2 - 15
+            ys3 = HEIGHT/2 - 75 - 40 - 40
+            pygame.draw.polygon(window, black, ((0+xs3,10+ys3),(0+xs3,20+ys3),(20+xs3,20+ys3),(20+xs3,25+ys3),(30+xs3,15+ys3),(20+xs3,5+ys3),(20+xs3,10+ys3)))
+            xs1 = WIDTH/2 + 14
+            ys1 = HEIGHT/2 + 45 + 40 + 40
+            pygame.draw.polygon(window, black, ((0+xs1,10+ys1),(0+xs1,20+ys1),(-20+xs1,20+ys1),(-20+xs1,25+ys1),(-30+xs1,15+ys1),(-20+xs1,5+ys1),(-20+xs1,10+ys1)))
+            
+
+            current = 2
+            delay = turn_time
+            
+        elif (current == 2): # vertical LT yellow
+            north_green.set_color(gray)
+            south_green.set_color(gray)
+            north_yellow.set_color(yellow_on)
+            south_yellow.set_color(yellow_on)
+            xs2 = WIDTH/2 - 15
+            ys2 = HEIGHT/2 - 75 - 40
+            pygame.draw.polygon(window, black, ((0+xs2,10+ys2),(0+xs2,20+ys2),(20+xs2,20+ys2),(20+xs2,25+ys2),(30+xs2,15+ys2),(20+xs2,5+ys2),(20+xs2,10+ys2)))
+            xs1 = WIDTH/2 + 14
+            ys1 = HEIGHT/2 + 45 + 40 
+            pygame.draw.polygon(window, black, ((0+xs1,10+ys1),(0+xs1,20+ys1),(-20+xs1,20+ys1),(-20+xs1,25+ys1),(-30+xs1,15+ys1),(-20+xs1,5+ys1),(-20+xs1,10+ys1)))
+            
+            current = 3
+            delay = yellow_time
+
+        elif (current == 3): # vertical LT red
+            north_yellow.set_color(gray)
+            south_yellow.set_color(gray)
+            north_red.set_color(red_on)
+            south_red.set_color(red_on)
+            xs1 = WIDTH/2 - 15
+            ys1 = HEIGHT/2 - 75
+            pygame.draw.polygon(window, black, ((0+xs1,10+ys1),(0+xs1,20+ys1),(20+xs1,20+ys1),(20+xs1,25+ys1),(30+xs1,15+ys1),(20+xs1,5+ys1),(20+xs1,10+ys1)))
+            xs1 = WIDTH/2 + 14
+            ys1 = HEIGHT/2 + 45
+            pygame.draw.polygon(window, black, ((0+xs1,10+ys1),(0+xs1,20+ys1),(-20+xs1,20+ys1),(-20+xs1,25+ys1),(-30+xs1,15+ys1),(-20+xs1,5+ys1),(-20+xs1,10+ys1)))
+            
+            current = 4
+            delay = pedestrian_time
+
+        elif (current == 4): #North and South Green
+            north_back_green.set_color(green_on)
+            south_back_green.set_color(green_on)
 
             north_back_red.set_color(gray)
             south_back_red.set_color(gray)
 
+            # Pedestrian Lights
             pygame.draw.rect(window, green_on, southwest_vertical)
             pygame.draw.rect(window, green_on, northwest_vertical)
             pygame.draw.rect(window, green_on, southeast_vertical)
@@ -250,18 +334,12 @@ def run_intersection():
             pygame.draw.rect(window, red_on, southwest_horizontal)
             pygame.draw.rect(window, red_on, southeast_horizontal)
 
-            current = 2
+            current = 5
             delay = go_time
 
-        elif (current == 2): #North and South Yellow
-            north_yellow.set_color(yellow_on)
-            south_yellow.set_color(yellow_on)
-
+        elif (current == 5): #North and South Yellow
             north_back_yellow.set_color(yellow_on)
             south_back_yellow.set_color(yellow_on)
-
-            north_green.set_color(gray)
-            south_green.set_color(gray)
 
             north_back_green.set_color(gray)
             south_back_green.set_color(gray)
@@ -291,27 +369,19 @@ def run_intersection():
                 
             delay = 0
 
-            current = 3
+            current = 6
 
-        elif (current == 3): #All Red
-            north_red.set_color(red_on)
-            south_red.set_color(red_on)
-
+        elif (current == 6): #All Red
             north_back_red.set_color(red_on)
             south_back_red.set_color(red_on)
-
-            west_red.set_color(red_on)
-            east_red.set_color(red_on)
 
             west_back_red.set_color(red_on)
             east_back_red.set_color(red_on)
 
-            north_yellow.set_color(gray)
-            south_yellow.set_color(gray)
-
             north_back_yellow.set_color(gray)
             south_back_yellow.set_color(gray)
 
+            # Pedestrian Lights
             pygame.draw.rect(window, red_on, southwest_vertical)
             pygame.draw.rect(window, red_on, northwest_vertical)
             pygame.draw.rect(window, red_on, southeast_vertical)
@@ -322,35 +392,69 @@ def run_intersection():
             pygame.draw.rect(window, red_on, southwest_horizontal)
             pygame.draw.rect(window, red_on, southeast_horizontal)
 
-            current = 4
+            current = 7
+            delay = pedestrian_time
+
+        elif (current == 7): # horizontal LT green
+            east_green.set_color(green_on)
+            west_green.set_color(green_on)
+            east_red.set_color(gray)
+            west_red.set_color(gray)
+            xs1 = WIDTH/2 - 75 - 40 - 40
+            ys1 = HEIGHT/2 - 15
+            pygame.draw.polygon(window, black, ((10+xs1,0+ys1),(20+xs1,0+ys1),(20+xs1,20+ys1),(25+xs1,20+ys1),(15+xs1,30+ys1),(5+xs1,20+ys1),(10+xs1,20+ys1)))
+            xs1 = WIDTH/2 + 75 + 40 + 40
+            ys1 = HEIGHT/2 - 15
+            pygame.draw.polygon(window, black, ((-10+xs1,0+ys1),(-20+xs1,0+ys1),(-20+xs1,20+ys1),(-25+xs1,20+ys1),(-15+xs1,30+ys1),(-5+xs1,20+ys1),(-10+xs1,20+ys1)))
+
+            current = 8
+            delay = turn_time
+            
+        elif (current == 8): # horizontal LT yellow
+            east_green.set_color(gray)
+            west_green.set_color(gray)
+            east_yellow.set_color(yellow_on)
+            west_yellow.set_color(yellow_on)
+            xs1 = WIDTH/2 - 75 - 40
+            ys1 = HEIGHT/2 - 15
+            pygame.draw.polygon(window, black, ((10+xs1,0+ys1),(20+xs1,0+ys1),(20+xs1,20+ys1),(25+xs1,20+ys1),(15+xs1,30+ys1),(5+xs1,20+ys1),(10+xs1,20+ys1)))
+            xs1 = WIDTH/2 + 75 + 40
+            ys1 = HEIGHT/2 - 15
+            pygame.draw.polygon(window, black, ((-10+xs1,0+ys1),(-20+xs1,0+ys1),(-20+xs1,20+ys1),(-25+xs1,20+ys1),(-15+xs1,30+ys1),(-5+xs1,20+ys1),(-10+xs1,20+ys1)))
+
+            current = 9
+            delay = yellow_time
+
+        elif (current == 9): # horizontal LT red
+            east_yellow.set_color(gray)
+            west_yellow.set_color(gray)
+            east_red.set_color(red_on)
+            west_red.set_color(red_on)
+            xs1 = WIDTH/2 - 75
+            ys1 = HEIGHT/2 - 15
+            pygame.draw.polygon(window, black, ((10+xs1,0+ys1),(20+xs1,0+ys1),(20+xs1,20+ys1),(25+xs1,20+ys1),(15+xs1,30+ys1),(5+xs1,20+ys1),(10+xs1,20+ys1)))
+            xs1 = WIDTH/2 + 75
+            ys1 = HEIGHT/2 - 15
+            pygame.draw.polygon(window, black, ((-10+xs1,0+ys1),(-20+xs1,0+ys1),(-20+xs1,20+ys1),(-25+xs1,20+ys1),(-15+xs1,30+ys1),(-5+xs1,20+ys1),(-10+xs1,20+ys1)))
+
+            current = 10
             delay = pedestrian_time
 
 
-        elif (current == 4): #East and West Green
-            west_green.set_color(green_on)
-            east_green.set_color(green_on)
-
+        elif (current == 10): #East and West Green
             west_back_green.set_color(green_on)
             east_back_green.set_color(green_on)
-
-            west_red.set_color(gray)
-            east_red.set_color(gray)
 
             west_back_red.set_color(gray)
             east_back_red.set_color(gray)
 
-            north_red.set_color(red_on)
-            south_red.set_color(red_on)
-
             north_back_red.set_color(red_on)
             south_back_red.set_color(red_on)
-
-            north_yellow.set_color(gray)
-            south_yellow.set_color(gray)
 
             north_back_yellow.set_color(gray)
             south_back_yellow.set_color(gray)
 
+            # Pedestrian Lights
             pygame.draw.rect(window, red_on, southwest_vertical)
             pygame.draw.rect(window, red_on, northwest_vertical)
             pygame.draw.rect(window, red_on, southeast_vertical)
@@ -361,18 +465,12 @@ def run_intersection():
             pygame.draw.rect(window, green_on, southwest_horizontal)
             pygame.draw.rect(window, green_on, southeast_horizontal)
 
-            current = 5
+            current = 11
             delay = go_time
-        elif (current == 5): #East and West Yellow
-            west_yellow.set_color(yellow_on)
-            east_yellow.set_color(yellow_on)
-
+        elif (current == 11): #East and West Yellow
             west_back_yellow.set_color(yellow_on)
             east_back_yellow.set_color(yellow_on)
-
-            west_green.set_color(gray)
-            east_green.set_color(gray)
-
+            
             west_back_green.set_color(gray)
             east_back_green.set_color(gray)
 
